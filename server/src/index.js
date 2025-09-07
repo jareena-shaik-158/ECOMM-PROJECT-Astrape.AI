759b3b9bea43bf4153d27f2c0271b0c1acefe98c
Shaik Jareena <shaikjareena233@gmail.com>
2025-09-07 11:44:27 +0530

    changes

--- server/src/index.js
+++ server/src/index.js
@@ -23,6 +23,9 @@ app.use("/api/cart", cartRoutes);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
-
+app.use(express.static(path.join(__dirname, 'client/dist'))); // Vite → dist, React → build
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
