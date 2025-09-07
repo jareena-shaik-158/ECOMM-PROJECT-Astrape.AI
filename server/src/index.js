f135ccffca8648f293667d8ed9d6120354f3ee92
Shaik Jareena <124500659+jareena-shaik-158@users.noreply.github.com>
2025-09-07 12:15:37 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,11 @@ const app = express();
 app.use(cors());
 app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
-
+app.use(cors({
+    origin: "https://ecomm-project-astrape-ai-7.onrender.com", // frontend ka URL
+    methods: ['GET','POST','PUT','DELETE'],
+    credentials: true
+}));
 // Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/items", itemRoutes);
